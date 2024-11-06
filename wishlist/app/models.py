aef7a75c6fa960bc171880d06f4bf030df28ea5d
Zhuravleva0701 <shpachevamariia@gmail.com>
2024-11-06 20:42:07 +0300

    обновлено 6.11

---
+++ wishlist/app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Item(models.Model):
+    title = models.CharField('Название хотелки', max_length=300)
+    description = models.CharField('Подробное описание', max_length=500)
+    price = models.IntegerField()
+    is_complete = models.BooleanField('Закрыто', default=False)
+
+    class Meta:
+        verbose_name = 'Желание'
+        verbose_name_plural = ('Желания')
+
+    def __str__(self):
+        return self.title
---
+++ wishlist/app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Item
+from django.views.decorators.http import require_http_methods
+
+
+def index(request):
+    wishlist = Item.objects.all()
+    return render(request, 'wishlist/index.html', {'wishlist': wishlist, 'title': 'Главная страница'})
+
+
+@require_http_methods(['POST'])
+def add(request):
+    title = request.POST['title']
+    description = request.POST['description']
+    price = request.POST['price']
+    item = Item(title=title, description=description, price=price)
+    item.save()
+    return redirect('index')
+
+
+def update(request, item_id):
+    item = Item.objects.get(id=item_id)
+    item.is_complete = not item.is_complete
+    item.save()
+    return redirect('index')
+
+
+def delete(request, item_id):
+    item = Item.objects.get(id=item_id)
+    item.delete()
+    return redirect('index')
